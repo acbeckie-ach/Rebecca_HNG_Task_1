8a8627e1a5d26227af6c21ce9bf89c7d53738647
acbeckie-ach <58997748+acbeckie-ach@users.noreply.github.com>
2023-09-12 16:59:49 +0300

    Update test.js

--- Task_1/test.js
+++ Task_1/test.js
@@ -1,37 +1,17 @@
-// display name
-function userName () {
-    document.getElementById("slackUserName").innerHTML = "Rebecca Acheampong"; 
-}
-
-userName();
-
-//display picture 
-function getImage() {
-    var myImage = document.createElement("img");
-    myImage.src = "meeee.png";
-    var src = document.getElementById("slackDisplayImage");
-    src.appendChild(myImage);
-}
-getImage();
-
-//display current day
-
+//day of week
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 var currentDate = new Date();
 var currentDayIndx = currentDate.getDay();
 var currentDayOfWeek = weekday[currentDayIndx];
-document.getElementById("currentDayOfTheWeek").textContent=currentDayOfWeek;
+document.getElementById("day").textContent=currentDayOfWeek;
 
 //Track Display
 document.getElementById("myTrack").innerHTML = "Frontend";
 
 //current utc time
 var dTime = new Date();
-var hour = dTime.getUTCHours();
-var minutes = dTime.getMinutes();
-var seconds = dTime.getSeconds();
-utc = hour + ":" + minutes + ":" + seconds;
-document.getElementById("currentUTCTime").innerHTML = utc;
+var ttime = dTime.getTime();
+document.getElementById("time").innerHTML = ttime;
 
 
